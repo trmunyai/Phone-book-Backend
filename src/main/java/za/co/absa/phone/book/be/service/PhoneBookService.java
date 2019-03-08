59f9f2c1f05a5d982420aa39cc8caf58bb636083
tendani.munyai <tendani.munyai@cgm.com>
2019-03-08 10:19:20 +0200

    Added some comments

--- src/main/java/za/co/absa/phone/book/be/service/PhoneBookService.java
+++ src/main/java/za/co/absa/phone/book/be/service/PhoneBookService.java
@@ -3,5 +3,4 @@ package za.co.absa.phone.book.be.service;
 import za.co.absa.phone.book.be.model.PhoneBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhoneBookService extends JpaRepository<PhoneBook, Long>{
-}
+public interface PhoneBookService extends JpaRepository<PhoneBook, Long>{}
